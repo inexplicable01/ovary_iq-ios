d590d5f458db03dc2fa9e55974c61b2731bafc06
Reetu Rathore <reetu.rathore@mobcoder.com>
2022-02-18 10:44:52 +0530

    gitKeep file add

--- OvaryIQ/OvaryIQ/Module/Questions/RegularIrregularCycle/Controller/RegularOrIrregularCycleVC.swift
+++ OvaryIQ/OvaryIQ/Module/Questions/RegularIrregularCycle/Controller/RegularOrIrregularCycleVC.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-import FSCalendar
-
 class RegularOrIrregularCycleVC: UIViewController {
     // MARK: - IBOutlets
 
--- OvaryIQ/OvaryIQ/Module/Questions/StartDateOfYourPeriod/Controller/PeriodStartDateVC.swift
+++ OvaryIQ/OvaryIQ/Module/Questions/StartDateOfYourPeriod/Controller/PeriodStartDateVC.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-import FSCalendar
-
 class PeriodStartDateVC: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet private weak var lblYear: UILabel!
